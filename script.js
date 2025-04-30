88a0ffd031cdf15b2d7f997c2491a61181ac296f
kevinverduzco27 <kevin.verduzco27@gmail.com>
2025-04-30 12:02:34 -0700

    added font aswom icons and removed script

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 console.log("JavaScript is linked and running!");
-
-document.addEventListener("DOMContentLoaded", () => {
-    const heading = document.createElement("h1");
-    heading.textContent = "Hello from JavaScript!";
-    document.body.appendChild(heading);
-});
